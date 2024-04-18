87296cb384030f4b24422b50e790ac65c2c4d86a
Angel <aboahangel16@gmail.com>
2024-04-18 20:54:44 +0200

    some adjustments made to the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Created by A.A Designs</p>
+      <p>&copy; 2024 All Rights Reserved A.A Designs</p>
     </footer>
   );
 };
